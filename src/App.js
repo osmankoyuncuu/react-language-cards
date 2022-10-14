b40d747c4066fc005f1f55181b7b68dc1c79b015
Osman KOYUNCU <osmannnkoyuncuuu@gmail.com>
2022-10-14 23:24:25 +0300

    finish

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import reactImg from "./assets/react.svg";
 import data from "./helper/data";
 import Card from "./component/card/Card";
+import { v4 as uuid } from "uuid";
 
 const App = () => {
   return (
@@ -11,7 +12,12 @@ const App = () => {
         <h1>Language</h1>
         <div className="card-box">
           {data.map((item) => (
-            <Card name={item.name} img={item.img} options={item.options} />
+            <Card
+              name={item.name}
+              img={item.img}
+              options={item.options}
+              key={uuid()}
+            />
           ))}
         </div>
       </div>
